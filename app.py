fab223116ef32b524dfb806c29b090ce2987e020
Thales Bergamin <thalesbergamim@gmail.com>
2025-03-21 22:16:21 -0300

    Implementando Rag e Ajustes

--- app.py
+++ app.py
@@ -6,26 +6,39 @@ app = Flask(__name__)
 
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
-    data = request.json
-    print(f'EVENTO RECEBIDO: {data}')
     
-    waha = Waha()
-    ai_bot = AIBot()
+    data = request.json
     chat_id = data['payload']['from']
     received_message = data['payload']['body']
-    
+    is_group = '@g.us' in chat_id
+
+    if is_group:
+        return jsonify({'status': 'success', 'message': 'Mensagem de grupo ignorada.'}), 200
+
+    waha = Waha()
+    ai_bot = AIBot()
+
     waha.start_typing(chat_id=chat_id)
-    
-    response = ai_bot.invoke(question=received_message)
-    
+
+    history_messages = waha.get_history_messages(
+        chat_id=chat_id,
+        limit=10,
+    )
+
+    response_message = ai_bot.invoke(
+        history_messages=history_messages,
+        question=received_message,
+    )
+
     waha.send_message(
         chat_id=chat_id,
-        message=response,
+        message=response_message,
     )
-    
+
     waha.stop_typing(chat_id=chat_id)
-    
+
     return jsonify({'status': 'success'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- services/waha.py
+++ services/waha.py
@@ -3,8 +3,9 @@ import requests
 class Waha:
     def __init__(self):
         self.__api_url = 'http://waha:3000'
-
+        
     def send_message(self, chat_id, message):
+
         url = f'{self.__api_url}/api/sendText'
         headers = {
             'Content-Type': 'application/json',
@@ -20,7 +21,20 @@ class Waha:
             headers=headers,
         )
 
+    def get_history_messages(self, chat_id, limit):
+
+        url = f'{self.__api_url}/api/default/chats/{chat_id}/messages?limit={limit}&downloadMedia=false'
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        response = requests.get(
+            url=url,
+            headers=headers,
+        )
+        return response.json()
+    
     def start_typing(self, chat_id):
+
         url = f'{self.__api_url}/api/startTyping'
         headers = {
             'Content-Type': 'application/json',
@@ -34,8 +48,9 @@ class Waha:
             json=payload,
             headers=headers,
         )
-
+    
     def stop_typing(self, chat_id):
+
         url = f'{self.__api_url}/api/stopTyping'
         headers = {
             'Content-Type': 'application/json',
